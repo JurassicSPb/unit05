36b6c28be0345fa9e763cc1f4e138be3e9ed5a07
JurassicSPb[credential] <Jurassic5@yandex.ru>
2017-03-06 11:24:24 +0300

    added new methods and tests

--- src/main/java/com/epam/java/se/task1/FileBrowser.java
+++ src/main/java/com/epam/java/se/task1/FileBrowser.java
@@ -3,12 +3,13 @@ package com.epam.java.se.task1;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FilenameFilter;
+import java.nio.file.NotDirectoryException;
+import java.util.NoSuchElementException;
 
 /**
  * Created by Мария on 06.03.2017.
  */
 public class FileBrowser {
-    private File file;
     private String path;
 
 
@@ -44,4 +45,42 @@ public class FileBrowser {
             System.out.println("Directory not found");
         }
     }
+
+    public void goUp(File file) throws FileNotFoundException {
+        if (file.exists()) {
+            path = file.getParent();
+            System.out.println(path);
+        } else throw new FileNotFoundException("File not found");
+    }
+
+    public void goDown(File directory, String catalogue) {
+        try {
+            if (directory.isDirectory()) {
+                path = directory.getAbsolutePath() + "\\" + catalogue;
+                System.out.println(path);
+            }
+        } catch (NullPointerException e) {
+            System.out.println("Directory not found");
+        }
+    }
+
+    public void createNewDirectory(File directory) {
+        try {
+            if (!directory.exists()) {
+                directory.mkdir();
+                System.out.println("New directory created");
+            }
+        } catch (NullPointerException e){
+            System.out.println("Directory not found");
+        }
+    }
+
+    public void renameDirectory(File directory, File newDirectory) throws NotDirectoryException{
+        boolean isRenamed = directory.renameTo(newDirectory);
+        if (isRenamed || newDirectory.exists()){
+            System.out.println("Directory renamed");
+        }
+        else throw new NotDirectoryException("Wrong path or directory");
+    }
+
 }
---
+++ src/test/java/com/epam/java/se/task1/FileBrowserTest.java
@@ -0,0 +1,127 @@
+package com.epam.java.se.task1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.NotDirectoryException;
+
+/**
+ * Created by Мария on 06.03.2017.
+ */
+public class FileBrowserTest {
+    @Test
+            (expected = FileNotFoundException.class)
+    public void FileNotFoundExceptionTest() throws Exception {
+        File fake = new File("fake.txt");
+
+        FileBrowser browser = new FileBrowser();
+
+        browser.showAbsolutePath(fake);
+        browser.goUp(fake);
+
+    }
+
+    @Test
+    public void NullPointerExceptionCatchTest() throws Exception {
+        File fakeFile = new File("fake.txt");
+        File fakeDir = new File("\\fake");
+        String fakeCatalogue = "fake";
+
+        FileBrowser browser = new FileBrowser();
+
+        try {
+            browser.showDirectoryContent(fakeFile);
+            browser.searchOnlyTXTFiles(fakeFile);
+            browser.goDown(fakeDir, fakeCatalogue);
+            browser.createNewDirectory(fakeDir);
+        } catch (NullPointerException e) {
+            assertEquals(e.getMessage(), "Directory not found");
+        }
+    }
+    @Test
+            (expected = NotDirectoryException.class)
+    public void NotDirectoryExceptionTest() throws Exception{
+        File fakeDir = new File("C:\\Users\\fake\\fake");
+        File fakeNewDir = new File("C:\\Users\\fake\\newFake");
+
+        FileBrowser browser = new FileBrowser();
+
+        browser.renameDirectory(fakeDir, fakeNewDir);
+
+    }
+    @Test
+    public void showAbsolutePathTest() throws Exception {
+        File compare = new File("testfile.txt");
+
+        FileBrowser browser = new FileBrowser();
+        browser.showAbsolutePath(compare);
+
+        assertTrue(compare.getAbsolutePath().equals("C:\\Users\\Мария\\IdeaProjects\\unit05\\testfile.txt"));
+    }
+
+    @Test
+    public void showDirectoryContentTest() throws Exception {
+        File directory = new File("D:\\Application");
+
+        FileBrowser browser = new FileBrowser();
+
+        browser.showDirectoryContent(directory);
+
+        assertTrue(directory.isDirectory());
+
+    }
+
+    @Test
+    public void searchOnlyTXTFilesTest() throws Exception {
+        File directory = new File(".\\");
+
+        FileBrowser browser = new FileBrowser();
+
+        browser.searchOnlyTXTFiles(directory);
+    }
+
+    @Test
+    public void goUpTest() throws Exception {
+        File directory = new File("C:\\Python27\\Doc");
+
+        FileBrowser browser = new FileBrowser();
+
+        browser.goUp(directory);
+    }
+
+    @Test
+    public void goDownTest() throws Exception {
+        File directory = new File("C:\\Python27");
+        String catalogue = "Doc";
+
+        FileBrowser browser = new FileBrowser();
+
+        browser.goDown(directory, catalogue);
+    }
+
+    @Test
+    public void createNewDirectoryTest() throws Exception {
+        File newDirectory = new File("C:\\Users\\Мария\\testDir");
+
+        FileBrowser browser = new FileBrowser();
+
+        browser.createNewDirectory(newDirectory);
+
+        assertTrue(newDirectory.exists());
+    }
+    @Test
+    public void renameDirectoryTest() throws Exception {
+        File directory = new File("C:\\Users\\Мария\\testDir");
+        File newDirectory = new File("C:\\Users\\Мария\\newTestDir");
+
+        FileBrowser browser = new FileBrowser();
+
+        browser.renameDirectory(directory, newDirectory);
+
+        assertTrue(newDirectory.exists());
+    }
+
+}
